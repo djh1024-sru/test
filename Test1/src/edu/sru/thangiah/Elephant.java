52ea985392de04f66dca2aa28a0e51ad11b59aba
djh1024-sru <djh1024@sru.edu>
2024-01-23 16:45:08 -0500

    update

---
+++ Test1/src/edu/sru/thangiah/Elephant.java
@@ -0,0 +1,16 @@
+package edu.sru.thangiah;
+
+public class Elephant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("Hello World1");
+		System.out.println("Hello World4");
+		
+	}
+
+}
+
+
